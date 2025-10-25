f27fe75ce336a8a82b5c855c16ee374c69b34983
Pugalendi A <39829202+PugalendiArumugam@users.noreply.github.com>
2025-10-25 18:26:01 +0530

    custom authentication 2

--- src/main/java/com/nexusai/SchoolAppSpring/config/SecurityConfig.java
+++ src/main/java/com/nexusai/SchoolAppSpring/config/SecurityConfig.java
@@ -3,6 +3,7 @@ package com.nexusai.SchoolAppSpring.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.HttpSecurityBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
@@ -11,10 +12,12 @@ import org.springframework.security.web.SecurityFilterChain;
 @Configuration
 @EnableWebSecurity
 public class SecurityConfig {
-
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
-
+        http.csrf(customizer-> customizer.disable());
+        http.authorizeHttpRequests(request -> request.anyRequest().authenticated());
+        http.formLogin(Customizer.withDefaults());
+        http.httpBasic(Customizer.withDefaults());    // This code disables the default form return.
         return http.build();
     }
 }
