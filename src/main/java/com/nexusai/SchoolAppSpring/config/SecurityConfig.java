a2f2fd652e57450bb38eaef7489c387a22f4b21f
Pugalendi A <39829202+PugalendiArumugam@users.noreply.github.com>
2025-10-26 13:43:28 +0530

    UserDetailsService

--- src/main/java/com/nexusai/SchoolAppSpring/config/SecurityConfig.java
+++ src/main/java/com/nexusai/SchoolAppSpring/config/SecurityConfig.java
@@ -7,6 +7,11 @@ import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.HttpSecurityBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
@@ -14,10 +19,40 @@ import org.springframework.security.web.SecurityFilterChain;
 public class SecurityConfig {
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
-        http.csrf(customizer-> customizer.disable());
-        http.authorizeHttpRequests(request -> request.anyRequest().authenticated());
-        http.formLogin(Customizer.withDefaults());
-        http.httpBasic(Customizer.withDefaults());    // This code disables the default form return.
-        return http.build();
+
+        return http
+                .csrf(customizer -> customizer.disable())
+                .authorizeHttpRequests(req -> req.anyRequest().authenticated())
+                .httpBasic(Customizer.withDefaults())
+                .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+                .build();
+
+//        http.csrf(customizer-> customizer.disable());
+//        http.authorizeHttpRequests(request -> request.anyRequest().authenticated());
+//        http.formLogin(Customizer.withDefaults());
+//        http.httpBasic(Customizer.withDefaults());    // This code disables the default form return.
+//        return http.build();
     }
+
+    @Bean
+    public UserDetailsService userDetailsService(){
+
+        UserDetails user1 = User
+                .withDefaultPasswordEncoder()
+                .username("pugal")
+                .password("p@123")
+                .roles("USER")
+                .build();
+
+        UserDetails user2 = User
+                .withDefaultPasswordEncoder()
+                .username("sarvesh")
+                .password("s@123")
+                .roles("ADMIN")
+                .build();
+
+        return new InMemoryUserDetailsManager(user1,user2);
+
+    }
+
 }
