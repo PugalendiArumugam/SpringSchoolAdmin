e8467dcf14fb17ed1780b4dfadae7ada6771d173
Pugalendi A <39829202+PugalendiArumugam@users.noreply.github.com>
2025-11-30 09:39:47 +0530

    Connected to Postgress UserAuthentication done

---
+++ src/main/java/com/nexusai/SchoolAppSpring/repo/UserRepo.java
@@ -0,0 +1,12 @@
+package com.nexusai.SchoolAppSpring.repo;
+
+import com.nexusai.SchoolAppSpring.model.Users;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface UserRepo extends JpaRepository<Users, Integer> {
+
+    Users findByUsername(String username);
+
+}
--- src/main/java/com/nexusai/SchoolAppSpring/service/MyUserDetailService.java
+++ src/main/java/com/nexusai/SchoolAppSpring/service/MyUserDetailService.java
@@ -1,10 +1,35 @@
 package com.nexusai.SchoolAppSpring.service;
 
+import com.nexusai.SchoolAppSpring.model.UserPrincipal;
+import com.nexusai.SchoolAppSpring.model.Users;
+import com.nexusai.SchoolAppSpring.repo.UserRepo;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
 
+@Service
 public class MyUserDetailService implements UserDetailsService {
 
+    @Autowired
+    private UserRepo repo;
 
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+
+        Users user = repo.findByUsername(username);
+
+        if(user==null){
+            System.out.println("User not found");
+            throw new UsernameNotFoundException("user not found-exception");
+        } else {
+            System.out.println("User found !! ->"+username);
+        }
+
+        return new UserPrincipal(user);
+    }
 
 
 }
